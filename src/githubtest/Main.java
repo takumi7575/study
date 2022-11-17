b34538ee988d085c1c4731efb58cca6fbdc2ab6e
takumi <takumi1379@outlook.jp>
2022-11-18 06:58:11 +0900

    first commit

---
+++ src/githubtest/Main.java
@@ -0,0 +1,16 @@
+package githubtest;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int ans = x + y;
+		System.out.println(ans);
+		System.out.println("hoge");
+	}
+
+}
